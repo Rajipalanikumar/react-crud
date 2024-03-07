2d98962a59728a81d5de9e4be3b7c4fdd2dec481
Rajipalanikumar <98646802+Rajipalanikumar@users.noreply.github.com>
2024-03-07 13:58:33 +0530

    Create users.js

---
+++ users.js
@@ -0,0 +1,24 @@
+const users=[
+    {
+id:"1",
+bname:"nnn",
+sname:"nn"
+
+},
+{
+    id:"2",
+    bname:"nnn",
+    sname:"nn"
+    
+    },
+    
+{
+    id:"3",
+    bname:"nnn",
+    sname:"nn"
+        }
+
+
+
+]
+export default users;
