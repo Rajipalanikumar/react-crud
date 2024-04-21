c74966ca086c8c0ffeff4c82928d4b92055d054e
Rajipalanikumar <98646802+Rajipalanikumar@users.noreply.github.com>
2024-04-21 12:01:25 +0530

    Updated users.js

--- users.js
+++ users.js
@@ -1,21 +1,30 @@
 const users=[
     {
 id:"1",
-bname:"nnn",
-sname:"nn"
+bname:"bbbb",
+sname:"zgh",
+takendate:"01/02/2024 03:30",
+returndate:"01/02/2024 03:30",
+img:"https://images.unsplash.com/photo-1607992922515-7e38329e65d4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww"
 
 },
 {
     id:"2",
-    bname:"nnn",
-    sname:"nn"
+    bname:"raji",
+    sname:"nn",
+    takendate:"01/02/2024 03:30",
+    returndate:"01/02/2024 03:30",
+    img:"https://images.unsplash.com/photo-1607992922515-7e38329e65d4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww"
     
     },
     
 {
     id:"3",
     bname:"nnn",
-    sname:"nn"
+    sname:"asdf",
+    takendate:"01/02/2024 03:30",
+returndate:"01/02/2024 03:30",
+    img:"https://images.unsplash.com/photo-1607992922515-7e38329e65d4?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww"
         }
 
 
